57afcf75b217a575006751254f128f7f13ddabd0
shanecranor <shanecranor@mines.edu>
2022-04-02 14:16:11 -0600

    switched to GLTF model loader

--- index.js
+++ index.js
@@ -1,120 +1,119 @@
 import * as THREE from 'three';
 
-			import Stats from './jsm/libs/stats.module.js';
+import Stats from './jsm/libs/stats.module.js';
 
-			import { OrbitControls } from './jsm/controls/OrbitControls.js';
-			import { FBXLoader } from './jsm/loaders/FBXLoader.js';
+import { OrbitControls } from './jsm/controls/OrbitControls.js';
+import { GLTFLoader } from './jsm/loaders/GLTFLoader.js';
 
-			let camera, scene, renderer, stats;
+let camera, scene, renderer, stats;
 
-			const clock = new THREE.Clock();
+const clock = new THREE.Clock();
 
-			let mixer;
+let mixer;
 
-			init();
-			animate();
+init();
+animate();
 
-			function init() {
+function init() {
 
-				const container = document.createElement( 'div' );
-				document.body.appendChild( container );
+	const container = document.createElement( 'div' );
+	document.body.appendChild( container );
 
-				camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 2000 );
-				camera.position.set( 100, 200, 300 );
+	camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 2000 );
+	camera.position.set( 100, 200, 300 );
 
-				scene = new THREE.Scene();
-				scene.background = new THREE.Color( 0xa0a0a0 );
-				scene.fog = new THREE.Fog( 0xa0a0a0, 200, 1000 );
+	scene = new THREE.Scene();
+	scene.background = new THREE.Color( 0xa0a0a0 );
+	scene.fog = new THREE.Fog( 0xa0a0a0, 200, 1000 );
 
-				const hemiLight = new THREE.HemisphereLight( 0xffffff, 0x444444 );
-				hemiLight.position.set( 0, 200, 0 );
-				scene.add( hemiLight );
+	const hemiLight = new THREE.HemisphereLight( 0xffffff, 0x444444 );
+	hemiLight.position.set( 0, 200, 0 );
+	scene.add( hemiLight );
 
-				const dirLight = new THREE.DirectionalLight( 0xffffff );
-				dirLight.position.set( 0, 200, 100 );
-				dirLight.castShadow = true;
-				dirLight.shadow.camera.top = 180;
-				dirLight.shadow.camera.bottom = - 100;
-				dirLight.shadow.camera.left = - 120;
-				dirLight.shadow.camera.right = 120;
-				scene.add( dirLight );
+	const dirLight = new THREE.DirectionalLight( 0xffffff );
+	dirLight.position.set( 0, 200, 100 );
+	dirLight.castShadow = true;
+	dirLight.shadow.camera.top = 180;
+	dirLight.shadow.camera.bottom = - 100;
+	dirLight.shadow.camera.left = - 120;
+	dirLight.shadow.camera.right = 120;
+	scene.add( dirLight );
 
-				// scene.add( new THREE.CameraHelper( dirLight.shadow.camera ) );
+	// scene.add( new THREE.CameraHelper( dirLight.shadow.camera ) );
 
-				// ground
-				const mesh = new THREE.Mesh( new THREE.PlaneGeometry( 2000, 2000 ), new THREE.MeshPhongMaterial( { color: 0x999999, depthWrite: false } ) );
-				mesh.rotation.x = - Math.PI / 2;
-				mesh.receiveShadow = true;
-				scene.add( mesh );
+	// ground
+	const mesh = new THREE.Mesh( new THREE.PlaneGeometry( 2000, 2000 ), new THREE.MeshPhongMaterial( { color: 0x999999, depthWrite: false } ) );
+	mesh.rotation.x = - Math.PI / 2;
+	mesh.receiveShadow = true;
+	scene.add( mesh );
 
-				const grid = new THREE.GridHelper( 2000, 20, 0x000000, 0x000000 );
-				grid.material.opacity = 0.2;
-				grid.material.transparent = true;
-				scene.add( grid );
+	const grid = new THREE.GridHelper( 2000, 20, 0x000000, 0x000000 );
+	grid.material.opacity = 0.2;
+	grid.material.transparent = true;
+	scene.add( grid );
 
-				// model
-				const loader = new FBXLoader();
-				loader.load( 'models/Model/Samba Dancing.fbx', function ( object ) {
-					object.scale.set(1.0, 1.0, 1.0)
-					// mixer = new THREE.AnimationMixer( object );
+	// model
+	const loader = new GLTFLoader();
+	loader.load( 'models/Model/tree.glb',
+		function ( gltf ) {
+			scene.add( gltf.scene );
+			gltf.animations; // Array<THREE.AnimationClip>
+			gltf.scene; // THREE.Group
+			gltf.scenes; // Array<THREE.Group>
+			gltf.cameras; // Array<THREE.Camera>
+			gltf.asset; // Object
+		},
+		// called while loading is progressing
+		function ( xhr ) {
+			console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
+		},
+		// called when loading has errors
+		function ( error ) {
 
-					// const action = mixer.clipAction( object.animations[ 0 ] );
-					// action.play();
+			console.log( 'An error happened' );
 
-					// object.traverse( function ( child ) {
+		}
+	);
 
-					// 	if ( child.isMesh ) {
+	renderer = new THREE.WebGLRenderer( { antialias: true } );
+	renderer.setPixelRatio( window.devicePixelRatio );
+	renderer.setSize( window.innerWidth, window.innerHeight );
+	renderer.shadowMap.enabled = true;
+	container.appendChild( renderer.domElement );
 
-					// 		child.castShadow = true;
-					// 		child.receiveShadow = true;
+	const controls = new OrbitControls( camera, renderer.domElement );
+	controls.target.set( 0, 100, 0 );
+	controls.update();
 
-					// 	}
+	window.addEventListener( 'resize', onWindowResize );
 
-					// } );
+	// stats
+	stats = new Stats();
+	container.appendChild( stats.dom );
 
-					scene.add( object );
+}
 
-				} );
+function onWindowResize() {
 
-				renderer = new THREE.WebGLRenderer( { antialias: true } );
-				renderer.setPixelRatio( window.devicePixelRatio );
-				renderer.setSize( window.innerWidth, window.innerHeight );
-				renderer.shadowMap.enabled = true;
-				container.appendChild( renderer.domElement );
+	camera.aspect = window.innerWidth / window.innerHeight;
+	camera.updateProjectionMatrix();
 
-				const controls = new OrbitControls( camera, renderer.domElement );
-				controls.target.set( 0, 100, 0 );
-				controls.update();
+	renderer.setSize( window.innerWidth, window.innerHeight );
 
-				window.addEventListener( 'resize', onWindowResize );
+}
 
-				// stats
-				stats = new Stats();
-				container.appendChild( stats.dom );
+//
 
-			}
+function animate() {
 
-			function onWindowResize() {
+	requestAnimationFrame( animate );
 
-				camera.aspect = window.innerWidth / window.innerHeight;
-				camera.updateProjectionMatrix();
+	const delta = clock.getDelta();
 
-				renderer.setSize( window.innerWidth, window.innerHeight );
+	if ( mixer ) mixer.update( delta );
 
-			}
+	renderer.render( scene, camera );
 
-			//
+	stats.update();
 
-			function animate() {
-
-				requestAnimationFrame( animate );
-
-				const delta = clock.getDelta();
-
-				if ( mixer ) mixer.update( delta );
-
-				renderer.render( scene, camera );
-
-				stats.update();
-
-			}
+}
